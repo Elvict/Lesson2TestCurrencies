94de120e300d113c210a01470a3a13a5de0b2c18
ER <elvict@gmail.com>
2016-08-14 00:55:24 +0300

    Initial commit

---
+++ src/test/java/FirstTestCurrency.java
@@ -0,0 +1,21 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+/**
+ * Created by Elena Romanovskaya
+ */
+public class FirstTestCurrency {
+
+    @Test
+    public void testName() throws Exception {
+
+    }
+}
